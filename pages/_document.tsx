b9a458608a98540c4f914b839856b1c21003869b
a133xz <22479344+a133xz@users.noreply.github.com>
2021-01-09 13:23:43 +0100

    document fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,10 @@
 import Document, { Head, Html, Main, NextScript } from "next/document"
 class MyDocument extends Document {
-  static getInitialProps ({ pathname, renderPage }) {
-    const {html} = renderPage()
-    return { pathname, html }
-  }
   render() {
     return (
       <Html lang="es">
         <Head />
-        <body className={this.props.pathname.replace("/", "")}>
+        <body>
           <Main />
           <NextScript />
         </body>
